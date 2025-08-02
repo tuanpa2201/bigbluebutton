5c3d7a2cadaefd5f89247d89f846ef3469eca8d4
hiepvv05 <167493438+hiepvuv@users.noreply.github.com>
2025-08-02 11:00:38 +0700

    Task: Quản lý cài đặt phiên học > Learning Dashboard

--- bbb-learning-dashboard/src/components/UsersTable.jsx
+++ bbb-learning-dashboard/src/components/UsersTable.jsx
@@ -332,7 +332,7 @@ class UsersTable extends React.Component {
                         </span>
                       ) : (<span>-</span>) }
                     </td>
-                    <td className={`px-4 py-3 text-sm text-center ${opacity}`} data-test="userTotalMessagesDashboard">
+                    <td className={`px-4 py-3 text-sm col-text-left ${opacity}`} data-test="userTotalMessagesDashboard">
                       { user.totalOfMessages > 0
                         ? (
                           <span>
